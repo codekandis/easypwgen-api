8d66c350e931ed89d646c5c96e6baf5c67fc37c3
Christian Ramelow <info@codekandis.net>
2025-11-18 14:09:29 +0100

    Updates the release version to `0.5.0`.

--- src/Configurations/Plain/sentryClient.php
+++ src/Configurations/Plain/sentryClient.php
@@ -8,6 +8,6 @@ return [
 	'displayErrors' => false,
 	'errorTypes'    => E_ALL,
 	'environment'   => 'production',
-	'release'       => '0.4.0',
+	'release'       => '0.5.0',
 	'serverName'    => 'api.easypwgen.codekandis'
 ];
